37af9137cda8f11a8201ceaca65ba29ebe810e8c
victorAnumudu <anumuduchukwuebuka@gmail.com>
2020-02-21 12:57:55 +0100

    Added Show/Hide Button to Footer Links

---
+++ innerPages/personal/personal.js
@@ -0,0 +1,54 @@
+function Open1(){
+    document.getElementById("section8List1").style.display = "block";
+    document.getElementById("listHide1").style.zIndex = 2;
+    document.getElementById("listShow1").style.zIndex = 1;
+}
+function Close1(){
+    document.getElementById("section8List1").style.display = "none";
+    document.getElementById("listHide1").style.zIndex = 1;
+    document.getElementById("listShow1").style.zIndex = 2;
+}
+
+function Open2(){
+    document.getElementById("section8List2").style.display = "block";
+    document.getElementById("listHide2").style.zIndex = 2;
+    document.getElementById("listShow2").style.zIndex = 1;
+}
+function Close2(){
+    document.getElementById("section8List2").style.display = "none";
+    document.getElementById("listHide2").style.zIndex = 1;
+    document.getElementById("listShow2").style.zIndex = 2;
+}
+
+function Open3(){
+    document.getElementById("section8List3").style.display = "block";
+    document.getElementById("listHide3").style.zIndex = 2;
+    document.getElementById("listShow3").style.zIndex = 1;
+}
+function Close3(){
+    document.getElementById("section8List3").style.display = "none";
+    document.getElementById("listHide3").style.zIndex = 1;
+    document.getElementById("listShow3").style.zIndex = 2;
+}
+
+function Open4(){
+    document.getElementById("section8List4").style.display = "block";
+    document.getElementById("listHide4").style.zIndex = 2;
+    document.getElementById("listShow4").style.zIndex = 1;
+}
+function Close4(){
+    document.getElementById("section8List4").style.display = "none";
+    document.getElementById("listHide4").style.zIndex = 1;
+    document.getElementById("listShow4").style.zIndex = 2;
+}
+
+function Open5(){
+    document.getElementById("section8List5").style.display = "block";
+    document.getElementById("listHide5").style.zIndex = 2;
+    document.getElementById("listShow5").style.zIndex = 1;
+}
+function Close5(){
+    document.getElementById("section8List5").style.display = "none";
+    document.getElementById("listHide5").style.zIndex = 1;
+    document.getElementById("listShow5").style.zIndex = 2;
+}
