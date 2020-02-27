24396e5f4296672ae4fc4c7da19b2263401dbacf
victorAnumudu <anumuduchukwuebuka@gmail.com>
2020-02-27 11:47:57 +0100

    done with innerLink on navBar1

---
+++ innerPages/business/business.js
@@ -0,0 +1,76 @@
+function Open1(){
+    document.getElementById("section8List1").style.display = "block";
+    document.getElementById("listHide1").style.zIndex = 2;
+    document.getElementById("listShow1").style.zIndex = 1;
+}
+function Close1(){
+    document.getElementById("section8List1").style.display = "none";
+    document.getElementById("listHide1").style.zIndex = 1;
+    document.getElementById("listShow1").style.zIndex = 2;
+}
+
+function Open2(){
+    document.getElementById("section8List2").style.display = "block";
+    document.getElementById("listHide2").style.zIndex = 2;
+    document.getElementById("listShow2").style.zIndex = 1;
+}
+function Close2(){
+    document.getElementById("section8List2").style.display = "none";
+    document.getElementById("listHide2").style.zIndex = 1;
+    document.getElementById("listShow2").style.zIndex = 2;
+}
+
+function Open3(){
+    document.getElementById("section8List3").style.display = "block";
+    document.getElementById("listHide3").style.zIndex = 2;
+    document.getElementById("listShow3").style.zIndex = 1;
+}
+function Close3(){
+    document.getElementById("section8List3").style.display = "none";
+    document.getElementById("listHide3").style.zIndex = 1;
+    document.getElementById("listShow3").style.zIndex = 2;
+}
+
+function Open4(){
+    document.getElementById("section8List4").style.display = "block";
+    document.getElementById("listHide4").style.zIndex = 2;
+    document.getElementById("listShow4").style.zIndex = 1;
+}
+function Close4(){
+    document.getElementById("section8List4").style.display = "none";
+    document.getElementById("listHide4").style.zIndex = 1;
+    document.getElementById("listShow4").style.zIndex = 2;
+}
+
+function Open5(){
+    document.getElementById("section8List5").style.display = "block";
+    document.getElementById("listHide5").style.zIndex = 2;
+    document.getElementById("listShow5").style.zIndex = 1;
+}
+function Close5(){
+    document.getElementById("section8List5").style.display = "none";
+    document.getElementById("listHide5").style.zIndex = 1;
+    document.getElementById("listShow5").style.zIndex = 2;
+}
+
+function Open(){
+    document.getElementById("Nav2Item1").style.display = "block";
+    document.getElementById("Nav2ToggleIconOpen").style.display = "none";
+    document.getElementById("Nav2ToggleIconClose").style.display = "block";
+}
+function Close(){
+    document.getElementById("Nav2Item1").style.display = "none";
+    document.getElementById("Nav2ToggleIconOpen").style.display = "block";
+    document.getElementById("Nav2ToggleIconClose").style.display = "";
+}
+
+function Open22(){
+    document.getElementById("nav2Btn").style.display = "block";
+    document.getElementById("Nav2ToggleIconOpen2").style.display = "none";
+    document.getElementById("Nav2ToggleIconClose2").style.display = "block";
+}
+function Close22(){
+    document.getElementById("nav2Btn").style.display = "none";
+    document.getElementById("Nav2ToggleIconOpen2").style.display = "block";
+    document.getElementById("Nav2ToggleIconClose2").style.display = "";
+}
