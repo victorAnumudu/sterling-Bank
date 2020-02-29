3f106a3a691f15dd9b202671b6d3d37dd15f9372
victorAnumudu <anumuduchukwuebuka@gmail.com>
2020-02-29 11:05:28 +0100

    done with personal, business and alternative landing page

---
+++ images/innerPages/sterling/footer.php
@@ -0,0 +1,238 @@
+
+        <!-- section5 Kiki Background -->
+        <div class="section5">
+            <div class="section5Background row m-0">
+                <div class="section5Cover"></div>
+                <div class="section5Grid col-lg-12 col-xl-4">
+                    <h1 class="section5Header">Your SmartMoney <span class="section5HeaderBlack">Chat Buddy</span></h1>
+                    <p class="section5Para1">I’m Kiki – here to help</p>
+                    <p class="section5Para2">Now available on OnePay App</p>
+                    <div class="section5GetApp row mx-0 mt-5">
+                        <div class="section5App col-lg-2 col-xl-6 p-0">
+                            <a class="section5Link" href="">
+                                <img class="section5LinkItem" src="../sterling/images/section5/android-button.png" alt="android-button image">
+                            </a>
+                        </div>
+                        <div class="section5App col-lg-2 col-xl-6 p-0">
+                            <a class="section5Link" href="">
+                                <img class="section5LinkItem" src="../sterling/images/section5/ios-button.png" alt="android-button image">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end of section5 Kiki Background  -->
+
+         <!-- section6 Three Flex Display Images-->
+         <div class="section6 row no-gutters">
+            <!-- coolest Card -->
+            <div class="section6Item col-md p-0">
+                <a class="section6Anchor" href="">
+                    <img class="section6Img" src="images/section6/1.jpg" alt="Coolest Card Image">
+                </a>
+            </div>
+            <!-- end of coolest Card -->
+
+            <!-- Cafe one -->
+            <div class="section6Item col-md p-0">
+                <a class="section6Anchor" href="">
+                    <img class="section6Img" src="images/section6/2.jpg" alt="Coolest Card Image">
+                </a>
+            </div>
+            <!-- end of Cafe one -->
+
+            <!-- One Woman -->
+            <div class="section6Item col-md p-0">
+                <a class="section6Anchor" href="">
+                    <img class="section6Img" src="images/section6/3.jpg" alt="Coolest Card Image">
+                </a>
+            </div>
+            <!-- end of One Woman -->
+        </div>
+        <!-- end of section6 Three Flex Display Images -->
+
+
+        <!-- section7 Latest News Update-->
+        <div class="section7">
+            <h1 class="section7Head">Latest News</h1>
+            <p class="section7Para">Latest news, press related material, announcements and our media resources.</p>
+            
+            <div class="section7FlexCon row no-gutters">
+                <!-- Sterling Eat Drink Festival -->
+                <div class="section7Item col-xl p-0">
+                    <a class="section7Anchor" href="">
+                        <div class="section7Img section7Img1"></div>
+                        <div class="section7Description">
+                            <h1 class="section7ImgHeader">Sterling Eat Drink Festival, 2019 </h1>
+                            <p class="section7ImgPara">The Eat Drink Festival isn’t just a weekend to devour ‘delish’ delicacies – although that is unarguably the best thing about it! Hosting about 5,</p>
+                        </div>
+                    </a>
+                </div>
+                <!-- end of Sterling Eat Drink Festival -->
+    
+                <!-- Bridging the Gap for Coding -->
+                <div class="section7Item col-xl p-0">
+                    <a class="section7Anchor" href="">
+                        <div class="section7Img section7Img2"></div>
+                        <div class="section7Description">
+                            <h1 class="section7ImgHeader">Bridging the Gap for Coding </h1>
+                            <p class="section7ImgPara">In a world where disruptive technologies are changing the way people live and work, Raheem Akingbolu reports that in Nigeria, Sterling Bank is championing the</p>
+                        </div>
+                    </a>
+                </div>
+                <!-- end of Bridging the Gap for Coding -->
+    
+                <!-- #SterlingAtAkeFestival -->
+                <div class="section7Item col-xl p-0">
+                    <a class="section7Anchor" href="">
+                        <div class="section7Img section7Img3"></div>
+                        <div class="section7Description">
+                            <h1 class="section7ImgHeader">#SterlingAtAkeFestival: What Went Down! </h1>
+                            <p class="section7ImgPara">Africa’s literati at home and the diaspora are still gushing about the continent’s biggest and most colorful festival for writers, poets, thinkers, dancers, singers, actors</p>
+                        </div>
+                    </a>
+                </div>
+                <!-- #SterlingAtAkeFestival -->
+            </div>
+            <!-- end of section6 Three Flex Display Images -->
+            <a class="section7ReadMore" href="">Read More</a>
+        </div>
+        <!-- end of section7 Latest News Update -->
+
+        <!-- section8 Footer Div-->
+        <div class="section8">
+            <div class="section8LinkCon row no-gutters">
+                <ul class="section8List col-lg-2">
+                    <li class="section8ListItemHeader">Personal Banking <button onclick="Open1()" id="listShow1"><span>&plus;</span></button> <button onclick="Close1()" id="listHide1"><span>&minus;</span></button>
+                    </li>
+                    <ul id="section8List1">
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Savings</a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Current</a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Investment</a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Loans</a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Cards</a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">E-Channels</a>
+                        </li>
+                    </ul>
+                </ul>
+                <ul class="section8List col-lg-2">
+                    <li class="section8ListItemHeader">Business Banking <button onclick="Open2()" id="listShow2"><span>&plus;</span></button> <button onclick="Close2()" id="listHide2"><span>&minus;</span></button>
+                    </li>
+                    <ul id="section8List2">
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Corperates</a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">SME</a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Services</a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Payments</a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Loans</a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">H.E.A.R.T</a>
+                        </li>
+                    </ul>
+                </ul>
+                <ul class="section8List col-lg-2">
+                    <li class="section8ListItemHeader">Alternative Banking <button onclick="Open3()" id="listShow3"><span>&plus;</span></button> <button onclick="Close3()" id="listHide3"><span>&minus;</span></button>
+                    </li>
+                    <ul id="section8List3">
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">SAF <i class="section8Icon fas fa-arrow-right"></i></a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Private Banking <i class="section8Icon fas fa-arrow-right"></i></a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Invest Now</a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">ONE Education</a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Agriculture</a>
+                        </li>
+                    </ul>
+                </ul>
+                <ul class="section8List col-lg-2">
+                    <li class="section8ListItemHeader">About Us <button onclick="Open4()" id="listShow4"><span>&plus;</span></button> <button onclick="Close4()" id="listHide4"><span>&minus;</span></button>
+                    </li>
+                    <ul id="section8List4">
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Who we are</a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Awards</a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Board of Directors</a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Executive Management</a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Our Policy</a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Shareholders</a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Careers</a>
+                        </li>
+                    </ul>
+                </ul>
+                <ul class="section8List col-lg-2">
+                    <li class="section8ListItemHeader">Help & Support <button onclick="Open5()" id="listShow5"><span>&plus;</span></button> <button onclick="Close5()" id="listHide5"><span>&minus;</span></button>
+                    </li>
+                    <ul id="section8List5">
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Feed back</a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Contant us</a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Whistle Blower</a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">ATM Locator</a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Fail Transaction</a>
+                        </li>
+                        <li class="sectionListItem">
+                            <a class="section8Anchor" href="">Forex Report</a>
+                        </li>
+                    </ul>
+                </ul>
+            </div>
+            <div class="section8CopyRight row no-gutters">
+                <p class="section8CopyRightItem1 col-md-6 p-0">&copy; 2019 Sterling Bank. All Rights Reserved</p>
+                <p class="section8CopyRightItem2 col-md-6 p-0">Privacy Policy Terms and Conditions </p>
+            </div>
+        </div>
+        <!-- end of section8 Footer Div -->
+        <script src="personal.js" type="text/javaScript"></script>
+    </div>
+    <!-- end of containerwrapper -->
+
+</body>
+</html>
---
+++ images/innerPages/sterling/header-business.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Business - Sterling Bank</title>
+    <link rel="icon" href="https://sterling.ng/wp-content/uploads/2019/02/cropped-Sterling_favicon-32x32.png" type="x/icon">
+    <script src="https://kit.fontawesome.com/4ea96ace4f.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+     <link rel="stylesheet" href="../headerBusiness.css">
+    <link rel="stylesheet" href="../footer.css">
+    <link rel="stylesheet" href="../../css/bootstrap.css">
+</head>
+<body>
+
+    <!-- start of containerwrapper -->
+    <div class="container-fluid p-0">
+
+        <!-- landingSocial Links -->
+        <div class="landingSocial bg-danger">
+            <a href="https://www.facebook.com/SterlingBankNigeria" class="landingSocialIcon">
+                <i class="landingIcon fab fa-facebook-f"></i>
+            </a>
+            <a href="https://www.instagram.com/SterlingBank/" class="landingSocialIcon">
+                <i class="landingIcon fab fa-instagram"></i>
+            </a>
+            <a href="https://twitter.com/Sterling_bankng" class="landingSocialIcon">
+                <i class="landingIcon fab fa-twitter"></i>
+            </a>
+            <a href="https://www.youtube.com/user/SterlingBankng" class="landingSocialIcon">
+                <i class="landingIcon fab fa-youtube"></i>
+            </a>
+        </div>
+
+        <a href="https://api.whatsapp.com/send?phone=2349060006449&text=hello" class="landingChat1">
+            <img src="../../images/social/whatsapp.png" class="landingChat" alt="WhatsAppImage">
+        </a>
+        <a href="" class="landingChat2"><i class="landingChat fas fa-comments"></i></i></a>
+       <!-- end of landingSocial links -->
+
+        <!-- main content starts here! -->
+        <!-- Nav 1 starts here! -->
+        <nav class="nav1 row no-gutters">
+            <ul class="navItem1 row no-gutters m-0">
+                <li class="nav1List">
+                    <a href="personal.php" class="nav1LinkLeft">Personal</a>
+                </li>
+                <li class="nav1List">
+                    <a href="business.php" class="nav1LinkLeftActive">Business</a>
+                </li>
+                <li class="nav1List">
+                    <a href="alternative.php" class="nav1LinkLeft">Alternative</a>
+                </li>
+            </ul>
+
+            <ul class="navItem2 row no-gutters m-0">
+                <li class="nav1List2 nav1List2Inner">
+                    <a href="" class="nav1LinkRight nav1LinkInner">Investors</a>
+                    <div class="nav1InnerCon">
+                        <a href="" class="nav1InnerLink">Investors Overview</a>
+                        <a href="" class="nav1InnerLink">Investor Reports</a>
+                        <a href="" class="nav1InnerLink">Investor News</a>
+                        <a href="" class="nav1InnerLink">ShareHolders</a>
+                        <a href="" class="nav1InnerLink">Ratings</a>
+                    </div>
+                </li>
+                <li class="nav1List2">
+                    <a href="" class="nav1LinkRight">Non-Interest</a>
+                </li>
+                <li class="nav1List2">
+                    <a href="" class="nav1LinkRight">Private</a>
+                </li>
+                <li class="nav1List2">
+                    <a href="" class="nav1LinkRight">Diaspora</a>
+                </li>
+                <li class="nav1List2 nav1List2Inner">
+                    <a href="" class="nav1LinkRight nav1LinkInner">About Us</a>
+                    <div class="nav1InnerCon">
+                        <a href="" class="nav1InnerLink">Who we are</a>
+                        <a href="" class="nav1InnerLink">Awards</a>
+                        <a href="" class="nav1InnerLink">Board of Directors</a>
+                        <a href="" class="nav1InnerLink">Executive Management</a>
+                        <a href="" class="nav1InnerLink">Our policy</a>
+                        <a href="" class="nav1InnerLink">Careers</a>
+                        <a href="" class="nav1InnerLink">Events</a>
+                        <a href="" class="nav1InnerLink">Blog</a>
+                    </div>
+                </li>
+                <li class="nav1List2 nav1List2Inner">
+                    <a href="" class="nav1LinkRight nav1LinkInner">Support</a>
+                    <div class="nav1InnerCon">
+                        <a href="" class="nav1InnerLink">Contant Us</a>
+                        <a href="" class="nav1InnerLink">ATM Locator</a>
+                        <a href="" class="nav1InnerLink">FeedBack</a>
+                        <a href="" class="nav1InnerLink">Failed Transaction</a>
+                        <a href="" class="nav1InnerLink">Whistle Blower</a>
+                        <a href="" class="nav1InnerLink">Fraud</a>
+                        <a href="" class="nav1InnerLink">Brand-Guidelines</a>
+                    </div>
+                </li>
+            </ul>
+
+        </nav>
+        <!-- end of Nav 1 -->
+
+        <!-- start of Nav 2 -->
+        <div id="navWrapper">
+        <div class="nav2 row no-gutters">
+            <div class="nav2Image">
+                <img class="nav2Img" src="images/SterlingLogo.svg" alt="SterlingLogo">
+            </div>
+    
+            <div class="Navbar1 pl-4">
+                <div class="Nav2Toggle">
+                    <span class="Nav2ToggleCon">
+                        <button onclick="Open()" id="Nav2ToggleIconOpen"><i class="Nav2ToggleIcon fas fa-bars"></i>
+                        </button>
+                        <button onclick="Close()" id="Nav2ToggleIconClose"><i class="Nav2ToggleIcon fas fa-times">
+                        </i></button>
+                    </span>
+                </div>
+               
+                <nav class="Nav2Items row no-gutters" id="Nav2Item1">
+                        <a href="" class="nav2Link">Corporate</a>
+                        <a href="" class="nav2Link">SME</a>
+                        <a href="" class="nav2Link">Services</a>
+                        <a href="" class="nav2Link">Payment</a>
+                        <a href="" class="nav2Link">Loans</a>
+                        <a href="" class="nav2Link">H.E.A.R.T</a>
+                </nav>
+            </div>
+    
+            <div class="nav2Button">
+                <nav class="Nav2Search" id="Nav2Item2">
+                    <button onclick="OpenSearch()" class="nav2LinkIcon" href=""><i class="nav2Icon fas fa-search"></i></button>
+                    <!-- searchButton Div -->
+                        <div id="searchCon">
+                            <button onclick="CloseSearch()" id="searchClose">x</button>
+                            <input title="search" id="searchInput" type="text" placeholder="How can we help?" autofocus>
+                        </div>
+                    <!-- end of searchButton Div -->
+                </nav>
+                <div class="Nav2Toggle">
+                    <span class="Nav2ToggleCon">
+                        <button onclick="Open22()" id="Nav2ToggleIconOpen2"><i class="Nav2ToggleIcon fas fa-lock"></i>
+                        </button>
+                        <button onclick="Close22()" id="Nav2ToggleIconClose2"><i class="Nav2ToggleIcon fas fa-times">
+                        </i></button>
+                    </span>
+                </div>
+                <button id="nav2Btn"><i class="Nav2BtnIcon fas fa-lock"></i> Internet Banking
+                    <div id="nav2BtnInnerCon">
+                        <ul class="nav2BtnInner">
+                            <li class="nav2BtnItem">
+                                <a class="nav2BtnLink" href="../sterlingIB/sterlingIB.html" target="_blank">Sterling Onepay</a>
+                            </li>
+                            <li class="nav2BtnItem">
+                                <a class="nav2BtnLink" href="../sterlingIBS/sterlingIBS.html" target="_blank">Sterling IBS (Old)</a>
+                            </li>
+                        </ul>
+                    </div>
+                </button>
+            </div>
+        </div>
+        </div>
+        <!-- end of Nav 2 -->
+
+        <!-- section3 MainSlide -->
+        <div class="section3MainSlide">
+            <a class="section3SlideAnchor1" href="">
+                <div class="section3MainSlideCover"></div>
+                <div class="section3Description">
+                    <h1 class="section3AnchorLinkHeader">At the H.E.A.R.T of your Business</h1>
+                    <p class="section3AnchorLinkPara">Health, Education, Agriculture, Renewable Energy and Transportation section.
+                    </p>
+                    <input class="section3Btn" type="submit" value="Explore Offerings">
+                </div>
+            </a>
+
+             <a class="section3SlideAnchor1" href="">
+                <div class="section3MainSlideCover"></div>
+                <div class="section3Description">
+                    <h1 class="section3AnchorLinkHeader">Trade Just Got Easier</h1>
+                    <p class="section3AnchorLinkPara">Letter of Credit
+                    </p>
+                    <input class="section3Btn" type="submit" value="apply Online in 5 mins">
+                </div>
+            </a>
+
+            <a class="section3SlideAnchor1" href="">
+                <div class="section3MainSlideCover"></div>
+                <div class="section3Description">
+                    <h1 class="section3AnchorLinkHeader">Trade Solutions</h1>
+                    <p class="section3AnchorLinkPara">with a deep knowledge of global trade and export finance, we provide solutions to support your business.
+                    </p>
+                    <input class="section3Btn" type="submit" value="Get Started">
+                </div>
+            </a>
+            
+        </div>
+        <!-- end of section3 MainSlide -->
+       
+        <!-- section4 Save Investment banking -->
+        <div class="section4">
+            <div class="section4Con row no-gutters">
+                <!-- Corporate and Investment Banking -->
+                <div class="section4Save col-md-4 px-0">
+                    <div class="section4Header1">
+                        <div class="section4Overlay"></div>
+                        <p class="section4HeaderText">Corporate & Investment Banking</p>
+                        <div class="section4Link">
+                            <a class="section4Anchor" href="">Corporate/ <br> Investment</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- end of Corporate and Investment Banking -->
+
+                <!-- SME -->
+                <div class="section4Save col-md-4 px-0">
+                    <div class="section4Header2">
+                        <div class="section4Overlay"></div>
+                        <p class="section4HeaderText">SME</p>
+                        <div class="section4Link">
+                            <a class="section4Anchor" href="">SME</a>
+                            <a class="section4Anchor" href="">Payments</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- end of SME -->
+
+            </div>
+            <h1 class="section4Head">Business, Corporate and Investment Banking</h1>
+            <p class="section4Para">We believe in offering all our business customers an expert and personal level of service. From sole traders to international corporates, our Relationship managers will get to know your needs.</p>
+        </div>
+        <!-- end of section4 Save Investment banking -->
+
